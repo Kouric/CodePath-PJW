2a5a657ff91c43f0c3ebbc01c217ab53f9c8c13d
David Barcenas <90057009+dabarcenas6921@users.noreply.github.com>
2022-12-12 21:01:32 -0500

    Update Constants.swift

--- Spotifly/Constants.swift
+++ Spotifly/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct SpotifyConstants {
-    static let CLIENT_ID = "b934184dcb7a49998b363e237074d04a"
+    static let CLIENT_ID = ""
     static let SESSION_KEY = "spotifySessionKey"
     static let REDIRECT_URI = "http://mysite"
     static let SCOPE = "user-read-email"
